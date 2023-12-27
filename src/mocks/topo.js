3b4a1e1bc23c4408ddc67efdb776a7db4a59a32f
Gabriel Affonso <gabrielhaffonsop@gmail.com>
2023-12-26 23:52:17 -0300

    Adicionando o topo do app

--- src/mocks/topo.js
+++ src/mocks/topo.js
@@ -1,6 +1,6 @@
 const topo = {
     boasVindas: "Olá Gabriel",
-    legenda: "Encontre os melhores produtores da região!",
+    legenda: "Encontre os melhores produtores da região hoje!",
 };
 
 export default topo;
